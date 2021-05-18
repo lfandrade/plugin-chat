9c55f37ff85e71c0acc22b2685722bd000c1cbc9
Luis <luisfelipeandrade@gmail.com>
2021-05-17 23:46:26 -0300

    Add locale Brazilian Portuguese

---
+++ Locale/pt_BR/translations.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'There is no message.' => 'Não há mensagem.',
+    'Refresh Interval' => 'Intervalo de atualização',
+    'Period in second (3 seconds by default)' => 'Período em segundos (3 segundos por padrão)',
+    'Chat' => 'Chat',
+    'Minimalist Chat for Kanboard.' => 'Bate-papo minimalista para Kanboard.',
+);
